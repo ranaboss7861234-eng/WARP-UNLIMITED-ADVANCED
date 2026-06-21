f5402669e1ae2f84e146c331ba51e69894e16628
ranaboss7861234-eng <rana.boss7861234@gmail.com>
2026-06-22 01:53:47 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 ENV = False # or True
 INTERACTIVE_MODE = False # or True
-WARP_CLIENT_ID = ""
+WARP_CLIENT_ID = "brave-essence"
 SEND_LOG = False # or True
 HIDE_WC_ID = True # or False
 TELEGRAM_BOT_TOKEN = ""
